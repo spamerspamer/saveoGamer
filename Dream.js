73120071ca592b4fd061424f721921f611d36794
spamerspamer <45140505+spamerspamer@users.noreply.github.com>
2018-11-24 22:21:29 +0300

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,29 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("ٍSpam Bot By .ÈditeD#9997");
+
+function checks(){
+        setInterval(() => {
+            client.channels.get('508209174459449355').send('#credits');
+        }, 640000);
+}
+
+client.on("ready", () => {
+let channel =     client.channels.get("508710475203084328")
+setInterval(function() {
+channel.send('Armando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخةArmando 48651 fsdgdfhd645321f esfwefwe الالعغانتىم هيص بنصخثحبن خح نخ ةيصثبخة');
+}, 25)
+ checks();
+})
+ 
+
+
+client.on('message', message => {
+    let prefix = '11';
+   if(message.content.startsWith(prefix + 'say')) {
+       let args = message.content.split(' ').slice(1).join(' ');
+       message.channel.send(args)
+   } 
+});
+
+client.login("NTA4MjA2NzQ2NjQ5NzU1NjQ5.Dtly1Q.I-pfJDgrVs9idPHENilYvQmwSIM");
